80cc0e1aaaa42aa805259a515e1b032b714923da
Shah rahman <toshah.rahman@gmail.com>
2022-03-06 17:56:40 +0200

    update button componenet

--- cypress-react-playground/src/components/Button.js
+++ cypress-react-playground/src/components/Button.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
-const Button= ()=> <button>Click Me</button>
+const Button= ()=> {
 
+const [text,setText] = useState('')
+
+//<button>Click Me</button>
+
+return (
+    <button onClick={() => setText('Loading....')}>{text || 'Click Me'}</button>
+)
+
+}
 export default Button;
